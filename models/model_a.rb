af1e27320c47efe17564e93c225c0b620ec872fd
Joel Grant <joelmgrant@me.com>
2021-09-03 10:47:59 -0600

    Add dinosaur hash

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+dinosaur = {
+  "name" => "Tyrannosaurus",
+  "nickname" => "TRex",
+  "age" => 1000000,
+  "teeth" => 200,
+  "is_dangerous" => true,
+  "is_herbivore" => false,
+  "food_source" => ["You", "Me"],
+  "current_locations" => ["Jurassic Park"]
+
+}
