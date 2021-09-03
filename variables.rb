0ee4c0ebf810da527d56056cb11781445b6d40ef
Joel Grant <joelmgrant@me.com>
2021-09-03 10:33:24 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Die Hard"
+is_entertaining = true
+movie = "Star Wars"
+num_4 = 22
+num_6 = 87
+product = num_4 * num_6
+favorite_foods = ["Peach", "Cheese", "Cashew"]
+favorite_foods.shift()
+puts movie.length
