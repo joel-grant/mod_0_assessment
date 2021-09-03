983fc1e4c9bbf5703a41ae8579219da9f81b0d7d
Joel Grant <joelmgrant@me.com>
2021-09-03 10:54:38 -0600

    Add developer hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "name" => "Joel",
+  "location" => "Denver",
+  "age" => 33,
+  "weight" => 220.4,
+  "is_motivated" = >true,
+  "has_cat" => false,
+  "favorite_resources" => ["Google", "Stack Overflow"],
+  "programming_languages" => ["Java", "Ruby"]
+}
